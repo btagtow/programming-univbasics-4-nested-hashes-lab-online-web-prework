835219c9f3af178344d6a6ef0fde182e36ef0c35
Ben Tagtow <bentagtow@gmail.com>
2019-11-07 23:07:49 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -14,8 +14,7 @@ def hopper
           :known_for => "Unix",
           :languages => ["C"]
         }
-     }
-     return programmer_hash["grace_hopper"]
+        return programmer_hash["grace_hopper"]
 end
 
 def alan_kay_is_known_for
